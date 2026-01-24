ba79ab370dd6a1c868038d80c66b3b1dad76050f
thelazybastard <monishgiani@gmail.com>
2026-01-24 22:21:11 +0700

    some changes as well as CLI tool ready
    now it actually makes sure the folder exists after users input.
    placed main function at top.
    placed main_directory_path to main function since globally used.
    created check to see if users input the right parameters when booting tool

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 # TODO:
 #   - DO NOT FORGET TO CHANGE PATH ONCE WHOLE TOOL HAS BEEN MADE
 #   - DO NOT FORGET TO CHANGE TIMEDELTA IN find_archivable_items_in_path() WHEN DONE DEBUGGING AND TESTING
-#   - users define folder the tool will operate on
-#   - turn script into CLI tool
+#   - debug this stupid fucking bastard eating whore licking asshole of a program. FUCK FUCK FUCK FUCK FUCK
+#   - currently it ignores the cutoff date the stupid crap refuses to do a thing i say it just archives everything
+#   - idk why nothing works oh cruel world WHY THE FUCK DOES IT ASK FOR USER INPUT TWICE.
 #   - make a separate GUI tool
 
 
@@ -10,9 +11,23 @@ from pathlib import Path
 from datetime import datetime, timedelta
 import zipfile
 import shutil
+import sys
+
+# group the main functions
+def main():
+    # checks if users launched tool properly
+    if len(sys.argv) > 1 and sys.argv[1] == 'archivist':
+        print("Starting tool...")
+        #runs the main functions
+        # set directory path
+        main_directory_path = Path.home() / find_folder()
+        convert_old_files_to_zip(main_directory_path)
+        delete_original_uncompressed_files(main_directory_path)
+    else:
+        print("Type 'do-my-bidding archiver' to launch tool")
 
 # find the main path
-def is_in_right_folder():
+def find_folder():
     # start from root home directory
     home_path = Path.home().resolve()
     # take in the right and expected input from the user
@@ -21,10 +36,10 @@ def is_in_right_folder():
                            "Currently only supports folders in the User home directory: ")
         try:
             # convert the string input object into a Path object
-            user_path = Path(user_input).resolve()
+            user_path = (home_path / user_input).resolve()
 
             # check to see if inputted path exists in directory
-            if user_path.is_relative_to(home_path):
+            if user_path.is_relative_to(home_path) and user_path.exists():
                 return user_path.relative_to(home_path)
             else:
                 # if the path is valid but does not exist in directory then try again
@@ -33,32 +48,14 @@ def is_in_right_folder():
             # handles wrong path names
             print(f"Invalid path. Please try again. Error: {e}")
 
-# set directory path
-main_directory_path = Path.home() / is_in_right_folder()
-
-# ask users if they want the tool to run in the background or one time use on a particular folder
-tool_usage_period = input("Do you want this tool to run in the background until device shutdown? "
-                          "Otherwise it will only run until all relevant items have been archived. "
-                          "Select 'y' if you want quick one-time sorting"
-                          " y/n: "
-                          )
-
 # ask users if the tool should run on startup or nah
-windows_startup_choice = input("Do you want this tool to run on startup? "
-                               "Tool will need to be launched manually if not. "
-                               " y/n: ")
-
-# group the main functions
-def main():
-    convert_old_files_to_zip()
-    delete_original_uncompressed_files()
-
-
-
+#windows_startup_choice = input("Do you want this tool to run on startup? "
+#                               "Tool will need to be launched manually if not. "
+#                               " y/n: ")
 
 # create timestamps for when the file/folder was last used,modified or created.
 # only files that have been ALL unused, unmodified, and created a very long time ago can be archived
-def find_archivable_items_in_path():
+def find_archivable_items_in_path(main_directory_path):
     for item in main_directory_path.iterdir():
         # creates timestamps of directories and files
         if item.is_dir() or item.is_file():
@@ -72,12 +69,12 @@ def find_archivable_items_in_path():
 
             #combine the timestamps into one and get the latest date they were used#
             # how many days before the file needs to be archived
-            still_in_use = max(date_modified, date_accessed) - timedelta(seconds=5)
+            still_in_use = max(date_modified, date_accessed) - timedelta(days=180)
             return still_in_use
 
 
 # converts files and folders into zip files
-def convert_to_zip():
+def convert_to_zip(main_directory_path):
     # goes through every item in directory
     for item in main_directory_path.iterdir():
         # skips item if already a zip file
@@ -101,13 +98,13 @@ def convert_to_zip():
                 zipf.write(item, arcname=item.relative_to(main_directory_path)) # maintain folder structure in zip file
 
 
-def convert_old_files_to_zip():
-    if find_archivable_items_in_path():
-        convert_to_zip()
+def convert_old_files_to_zip(main_directory_path):
+    if find_archivable_items_in_path(main_directory_path):
+        convert_to_zip(main_directory_path)
 
-def delete_original_uncompressed_files():
+def delete_original_uncompressed_files(main_directory_path):
     for item in main_directory_path.iterdir():
-        if item.suffix != '.zip' and find_archivable_items_in_path():
+        if item.suffix != '.zip' and find_archivable_items_in_path(main_directory_path):
             if item.is_dir():
                 shutil.rmtree(item)
             elif item.is_file():
