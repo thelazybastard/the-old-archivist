ff068a0a0bc6e9a990bce55028f3eb3570d1395c
thelazybastard <monishgiani@gmail.com>
2026-01-24 20:55:09 +0700

    added error handling
    users cannot input folders outside of user home directory to prevent bricking the OS

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 # TODO:
 #   - DO NOT FORGET TO CHANGE PATH ONCE WHOLE TOOL HAS BEEN MADE
 #   - DO NOT FORGET TO CHANGE TIMEDELTA IN find_archivable_items_in_path() WHEN DONE DEBUGGING AND TESTING
-#   - runs indefinitely in background until users stop it
 #   - users define folder the tool will operate on
 #   - turn script into CLI tool
 #   - make a separate GUI tool
@@ -13,15 +12,48 @@ import zipfile
 import shutil
 
 # find the main path
-user_input = input("Enter directory name: ")
+def is_in_right_folder():
+    # start from root home directory
+    home_path = Path.home().resolve()
+    # take in the right and expected input from the user
+    while True:
+        user_input = input("Enter the name of the directory you wish to clean up. "
+                           "Currently only supports folders in the User home directory: ")
+        try:
+            # convert the string input object into a Path object
+            user_path = Path(user_input).resolve()
+
+            # check to see if inputted path exists in directory
+            if user_path.is_relative_to(home_path):
+                return user_path.relative_to(home_path)
+            else:
+                # if the path is valid but does not exist in directory then try again
+                print("Invalid path. Input a folder within the User home directory.")
+        except Exception as e:
+            # handles wrong path names
+            print(f"Invalid path. Please try again. Error: {e}")
+
 # set directory path
-main_directory_path = Path.home() / user_input
+main_directory_path = Path.home() / is_in_right_folder()
+
+# ask users if they want the tool to run in the background or one time use on a particular folder
+tool_usage_period = input("Do you want this tool to run in the background until device shutdown? "
+                          "Otherwise it will only run until all relevant items have been archived. "
+                          "Select 'y' if you want quick one-time sorting"
+                          " y/n: "
+                          )
+
+# ask users if the tool should run on startup or nah
+windows_startup_choice = input("Do you want this tool to run on startup? "
+                               "Tool will need to be launched manually if not. "
+                               " y/n: ")
 
 # group the main functions
 def main():
     convert_old_files_to_zip()
     delete_original_uncompressed_files()
-    
+
+
 
 
 # create timestamps for when the file/folder was last used,modified or created.
@@ -55,7 +87,8 @@ def convert_to_zip():
         # dynamic naming of zip file
         zip_name = main_directory_path / f'{item.stem}.zip'
 
-        # archives folder subdirectories in path and scans recursively in them
+        # archives folder subdirectories in path and scans recursively in them,
+        # I do not know why the fuck this stupid shit method is raising a fucking warning god I hate this IDE
         if item.is_dir():
             with zipfile.ZipFile(zip_name, 'w', zipfile.ZIP_DEFLATED, allowZip64=True) as zipf:
                 for file in item.rglob('*'):
