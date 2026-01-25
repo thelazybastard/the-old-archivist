fcf2b7ebf0d9aed1d5b1c890c2a68101cf82f1fa
thelazybastard <monishgiani@gmail.com>
2026-01-25 15:45:13 +0700

    fixed program ignoring cutoff date.
    i fucking hate coding. fuck this shit. had to restructure the whole fucking thing

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-# TODO:
-#   - DO NOT FORGET TO CHANGE PATH ONCE WHOLE TOOL HAS BEEN MADE
-#   - DO NOT FORGET TO CHANGE TIMEDELTA IN find_archivable_items_in_path() WHEN DONE DEBUGGING AND TESTING
-#   - debug this stupid fucking bastard eating whore licking asshole of a program. FUCK FUCK FUCK FUCK FUCK
-#   - currently it ignores the cutoff date the stupid crap refuses to do a thing i say it just archives everything
-#   - make a separate GUI tool
-
-
 from pathlib import Path
 from datetime import datetime, timedelta
 import zipfile
@@ -17,13 +9,13 @@ def main():
     # checks if users launched tool properly
     if len(sys.argv) > 1 and sys.argv[1] == 'archivist':
         print("Starting tool...")
-        #runs the main functions
         # set directory path
         main_directory_path = Path.home() / find_folder()
-        convert_old_files_to_zip(main_directory_path)
-        delete_original_uncompressed_files(main_directory_path)
+        # executes the main functions
+        items_to_archive = convert_old_files_to_zip(main_directory_path)
+        delete_original_uncompressed_files(main_directory_path, items_to_archive)
     else:
-        print("Type 'do-my-bidding archiver' to launch tool")
+        print("Type 'do-my-bidding archivist' to launch tool")
 
 # find the main path
 def find_folder():
@@ -54,65 +46,77 @@ def find_folder():
 
 # create timestamps for when the file/folder was last used,modified or created.
 # only files that have been ALL unused, unmodified, and created a very long time ago can be archived
-def find_archivable_items_in_path(main_directory_path):
-    for item in main_directory_path.iterdir():
-        # creates timestamps of directories and files
-        if item.is_dir() or item.is_file():
-        # creates a log of when they were last used
-            modified = item.stat().st_mtime
-            accessed = item.stat().st_atime
+def is_item_archivable(item):
+    # creates timestamps of directories and files
+    # creates a log of when they were last used
+    modified = item.stat().st_mtime
+    accessed = item.stat().st_atime
 
-            # converts into readable format
-            date_modified = datetime.fromtimestamp(modified)
-            date_accessed = datetime.fromtimestamp(accessed)
+    # converts into readable format
+    date_modified = datetime.fromtimestamp(modified)
+    date_accessed = datetime.fromtimestamp(accessed)
 
-            #combine the timestamps into one and get the latest date they were used#
-            # how many days before the file needs to be archived
-            still_in_use = max(date_modified, date_accessed) - timedelta(days=180)
-            return still_in_use
-    #finally the stupid fucking IDE gave no warnings
-    return None
+    #combine the timestamps into one and get the latest date they were used#
+    # how many days before the file needs to be archived
+    cutoff = datetime.now() - timedelta(seconds=20)
+    last_used = max(date_modified, date_accessed)
+    return last_used < cutoff
+
 
 # converts files and folders into zip files
-def convert_to_zip(main_directory_path):
+def convert_old_files_to_zip(main_directory_path):
+    # list all archivable items
+    items_to_archive = []
     # goes through every item in directory
     for item in main_directory_path.iterdir():
-        # skips item if already a zip file
-        if item.suffix == '.zip':
-            continue
+        # identify items to archive
+        if item.suffix != '.zip' and is_item_archivable(item):
+            # appends items to list
+            items_to_archive.append(item)
 
+        # validates if the item is old enough to get archived
+    for archivable_item in items_to_archive:
+        # Double-check the item still exists and hasn't been moved/deleted
+        if not archivable_item.exists():
+            continue
         # dynamic naming of zip file
-        zip_name = main_directory_path / f'{item.stem}.zip'
+        zip_name = main_directory_path / f'{archivable_item.stem}.zip'
 
         # archives folder subdirectories in path and scans recursively in them,
         # I do not know why the fuck this stupid shit method is raising a fucking warning god I hate this IDE
-        if item.is_dir():
+        if archivable_item.is_dir():
             with zipfile.ZipFile(zip_name, 'w', zipfile.ZIP_DEFLATED, allowZip64=True) as zipf:
-                for file in item.rglob('*'):
+                for file in archivable_item.rglob('*'):
                     if file.is_file():
-                        zipf.write(file, arcname=file.relative_to(item)) # maintain folder structure in zip file
+                        zipf.write(file, arcname=file.relative_to(archivable_item)) # maintain folder structure in zip file
 
-        # archives files in path
+            # archives files in path
         else:
             with zipfile.ZipFile(zip_name, 'w', zipfile.ZIP_DEFLATED,  allowZip64=True) as zipf:
-                zipf.write(item, arcname=item.relative_to(main_directory_path)) # maintain folder structure in zip file
-
-# converts old files to zip files
-def convert_old_files_to_zip(main_directory_path):
-    if find_archivable_items_in_path(main_directory_path):
-        convert_to_zip(main_directory_path)
+                zipf.write(archivable_item, arcname=archivable_item.name) # maintain folder structure in zip file
+    # returns the archivable items
+    return items_to_archive
 
 # deletes the original files that got compressed
-def delete_original_uncompressed_files(main_directory_path):
+def delete_original_uncompressed_files(main_directory_path, items_to_archive):
     # iterate through the directory and only looks for .zip files belonging to the items it archived
-    for item in main_directory_path.iterdir():
-        if item.suffix != '.zip' and find_archivable_items_in_path(main_directory_path):
-            # if the item is a directory, delete
-            if item.is_dir():
-                shutil.rmtree(item)
-            # if the item is a file, delete
-            elif item.is_file():
-                item.unlink()
+    for item in items_to_archive:
+        # Check if corresponding zip exists
+        zip_name = main_directory_path / f'{item.stem}.zip'
+
+        if zip_name.exists():
+            try:
+                # if the item is a directory, delete
+                if item.is_dir():
+                    shutil.rmtree(item)
+                    print(f"Deleted folder: {item.name}")
+                # if the item is a file, delete
+                elif item.is_file():
+                    item.unlink()
+                    print(f"Deleted file: {item.name}")
+            # raise error if it has trouble deleting
+            except Exception as e:
+                print(f"Error deleting {item.name}: {e}")
 
 
 
